7a374f696f9913b4d4ebf507e681fe260b775b0e
bryan-laipple <bjlaipple@gmail.com>
2017-07-31 22:37:49 -0600

    mock data

--- server/server.go
+++ server/server.go
@@ -3,35 +3,55 @@ package server
 import (
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
+	"github.com/rs/cors"
 )
 
 type Server *iris.Application
 
+func GetOne(ctx context.Context, list []Summary) {
+	id, err := ctx.Params().GetInt("id");
+	if err != nil {
+		ctx.StatusCode(400)
+	}
+	ctx.JSON(list[id-1])
+}
+
 func Start(port int) Server {
 	app := iris.Default()
 
-	// Method:   GET
-	// Resource: http://localhost:8080/
-	app.Handle("GET", "/", func(ctx context.Context) {
-		ctx.HTML("Hello world!")
+	corsOptions := cors.Options{
+		AllowedOrigins:   []string{"*"},
+		AllowCredentials: true,
+	}
+
+	corsWrapper := cors.New(corsOptions).ServeHTTP
+
+	app.WrapRouter(corsWrapper)
+
+	app.Get("/characters", func(ctx context.Context) {
+		ctx.JSON(context.Map{"characters": Characters})
 	})
 
-	// same as app.Handle("GET", "/ping", [...])
-	// Method:   GET
-	// Resource: http://context:8080/ping
-	app.Get("/ping", func(ctx context.Context) {
-		ctx.WriteString("pong")
+	app.Get("/characters/:id", func(ctx context.Context) {
+		GetOne(ctx, Characters)
 	})
 
-	// Method:   GET
-	// Resource: http://localhost:8080/hello
-	app.Get("/hello", func(ctx context.Context) {
-		ctx.JSON(context.Map{"message": "Hello iris web framework."})
+	app.Get("/planets", func(ctx context.Context) {
+		ctx.JSON(context.Map{"planets": Planets})
 	})
 
-	// http://localhost:8080
-	// http://localhost:8080/ping
-	// http://localhost:8080/hello
+	app.Get("/planets/:id", func(ctx context.Context) {
+		GetOne(ctx, Planets)
+	})
+
+	app.Get("/starships", func(ctx context.Context) {
+		ctx.JSON(context.Map{"starships": Starships})
+	})
+
+	app.Get("/starships/:id", func(ctx context.Context) {
+		GetOne(ctx, Starships)
+	})
+
 	app.Run(iris.Addr(":8080"))
 
 	return app;
