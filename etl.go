14440666949d2c3ec5964e03dc55ff13f75a9911
bryan-laipple <bjlaipple@gmail.com>
2017-08-06 00:24:02 -0600

    manual update of etl data

--- etl.go
+++ etl.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+
 	"github.com/bryan-laipple/star-wars-server/etl"
 )
+
+func print(filename string) {
+	jsonBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(jsonBytes))
+}
 
 func main() {
 	//etl.Extract()
 	etl.Load(etl.Transform("./etl/extracted.json"))
+	print("./etl/extracted.json")
 }
